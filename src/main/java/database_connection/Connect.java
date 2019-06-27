2dfb6d4bff4649a4aaa9b0fa9669e17bcd420e72
Dmytro Kononenko <dmytro_kononenko1@epam.com>
2019-06-27 18:20:06 +0300

    fixed getAppPropertyValueByName() method

--- src/main/java/database_connection/Connect.java
+++ src/main/java/database_connection/Connect.java
@@ -1,19 +1,48 @@
 package database_connection;
 
+import com.restapi.manager.PropertiesManager;
+
 import java.sql.*;
 
 public class Connect {
-    public static void main(String[] args) throws ClassNotFoundException, SQLException {
-        //Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-        String connectionUrl = "jdbc:sqlserver://localhost:1433;database=kramar_task1;integratedSecurity=true;";
-        Connection con = DriverManager.getConnection(connectionUrl);
-        Statement statement = con.createStatement();
-        ResultSet resultSet = statement.executeQuery("select * from pages");
-        while ( resultSet.next()){
-            System.out.println(resultSet.getString("p_id").trim() +" " + resultSet.getString("p_name"));
-        }
 
-//        String sqlUpdate = "insert into pages"
+    public static Connection createDbConnection(){
+        String connectionUrl = PropertiesManager.getAppPropertyValueByName("dbconnection","url");
+        Connection connnection = null;
+        try {
+            connnection = DriverManager.getConnection(connectionUrl);
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return connnection;
+    }
+
+    public static Statement createStatement(){
+        Statement statement = null;
+        try {
+            statement = createDbConnection().createStatement();
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+        return statement;
+    }
+
+
+
+    public static void main(String[] args) throws SQLException {
+        //Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
+//        String connectionUrl = PropertiesManager.getAppPropertyValueByName("dbconnection","url");
+//        Connection con = DriverManager.getConnection(connectionUrl);
+//        Statement statement = con.createStatement();
+//        ResultSet resultSet = statement.executeQuery("select * from employees");
+
+        ResultSet resultSet = Connect.createStatement().executeQuery("select * from employees");
+        while ( resultSet.next()){
+            System.out.println(resultSet.getString("first_name").trim() +" "
+                    + resultSet.getString("last_name").trim()
+                    +" " + resultSet.getString("position"));
+        }
+//        String sqlInsert = "insert into pages"
 //                + "(p_parent,p_name)"
 //                + "values ('1','new line text')";
 //        statement.executeUpdate(sqlUpdate);
