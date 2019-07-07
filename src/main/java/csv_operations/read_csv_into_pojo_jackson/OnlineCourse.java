7f9ba38af17c9a4cb7331a0ac5f7f4a6a9cccceb
DimaKononenko04 <deadpharaoh1@gmail.com>
2019-07-07 15:13:46 +0300

    Singleton

--- src/main/java/csv_operations/read_csv_into_pojo_jackson/OnlineCourse.java
+++ src/main/java/csv_operations/read_csv_into_pojo_jackson/OnlineCourse.java
@@ -1,20 +1,21 @@
 package csv_operations.read_csv_into_pojo_jackson;
 
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 import com.fasterxml.jackson.annotation.JsonProperty;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
+
 
 public class OnlineCourse {
     private static OnlineCourse onlineCourse;
 
     public static OnlineCourse getOnlineCourse(){
-        return new OnlineCourse();
+        if(onlineCourse == null){
+            onlineCourse = new OnlineCourse();
+        }
+        return onlineCourse;
     }
 
     @JsonProperty("Title")
@@ -23,4 +24,32 @@ public class OnlineCourse {
     private String author;
     @JsonProperty("Price")
     private int price;
+
+    private OnlineCourse(){
+
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
 }
--- src/main/java/csv_operations/read_csv_into_pojo_jackson/Person.java
+++ src/main/java/csv_operations/read_csv_into_pojo_jackson/Person.java
@@ -2,23 +2,59 @@ package csv_operations.read_csv_into_pojo_jackson;
 
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import lombok.Getter;
-import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
-@NoArgsConstructor
 @JsonPropertyOrder({"name", "surname", "shoesize", "gender"})
 public class Person {
     private static Person person;
 
     public static Person getPerson(){
-        return new Person();
+        if (person == null) {
+            person = new Person();
+        }
+        return person;
     }
 
     private String name;
     private String surname;
     private int shoesize;
     private String gender;
+
+    private Person() {
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getSurname() {
+        return surname;
+    }
+
+    public void setSurname(String surname) {
+        this.surname = surname;
+    }
+
+    public int getShoesize() {
+        return shoesize;
+    }
+
+    public void setShoesize(int shoesize) {
+        this.shoesize = shoesize;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
 
 }
