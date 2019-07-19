9ac6b91422b6c8c0bdcd7850a18ff4668a123fa8
Dmytro Kononenko <dmytro_kononenko1@epam.com>
2019-07-19 14:57:48 +0300

    deleted comments from Connect class

--- src/main/java/database_connection/Connect.java
+++ src/main/java/database_connection/Connect.java
@@ -8,7 +8,7 @@ import java.util.List;
 
 public class Connect {
 
-    private static Connection createDbConnection(){
+    public static Connection createDbConnection(){
         String connectionUrl = PropertiesManager.getAppPropertyValueByName("dbconnection","url");
         Connection connnection = null;
         try {
@@ -33,26 +33,20 @@ public class Connect {
 
     public static void main(String[] args) throws SQLException, ClassNotFoundException {
         Class.forName("com.microsoft.sqlserver.jdbc.SQLServerDriver");
-//        String connectionUrl = PropertiesManager.getAppPropertyValueByName("dbconnection","url");
-//        Connection con = DriverManager.getConnection(connectionUrl);
-//        Statement statement = con.createStatement();
-//        ResultSet resultSet = statement.executeQuery("select * from employees");
         List<Employee> employees = new ArrayList<>();
         ResultSet resultSet = Connect.createStatement().executeQuery("select * from employees");
         while ( resultSet.next()){
             Employee employee = new Employee();
-            employee.setEmployeeId(Integer.parseInt(resultSet.getString("employee_id").trim()));
+            employee.setEmployeeId(resultSet.getInt("employee_id"));
             employee.setFirstName(resultSet.getString("first_name").trim());
             employee.setLastName(resultSet.getString("last_name").trim());
             employee.setPosition(resultSet.getString("position").trim());
             employees.add(employee);
         }
-        System.out.println(employees.get(1).getFirstName());
-        System.out.println(employees.get(1).getEmployeeId());
-//        String sqlInsert = "insert into pages"
-//                + "(p_parent,p_name)"
-//                + "values ('1','new line text')";
-//        statement.executeUpdate(sqlUpdate);
+        resultSet.close();
+        System.out.println(employees.get(3).getEmployeeId());
+        System.out.println(employees.get(3).getFirstName());
+
     }
 
 }
