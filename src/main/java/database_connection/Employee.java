d462ab36329d85a2dd65a57e8bddc8ecf8aa1a84
Dmytro Kononenko <dmytro_kononenko1@epam.com>
2019-08-07 15:32:00 +0300

    usage of lombok  data annotation

--- src/main/java/database_connection/Employee.java
+++ src/main/java/database_connection/Employee.java
@@ -1,10 +1,8 @@
 package database_connection;
 
-import lombok.Getter;
-import lombok.Setter;
+import lombok.Data;
 
-@Getter
-@Setter
+@Data
 public class Employee {
     private int employeeId;
     private String firstName;
