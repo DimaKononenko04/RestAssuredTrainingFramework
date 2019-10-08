c703ae8af29ea36f36414055f937cbe7125b442f
Dmytro Kononenko <dmytro_kononenko1@epam.com>
2019-10-08 10:56:00 +0300

    Completed Sudoku validator

--- src/main/java/codewars/SudokuValidator.java
+++ src/main/java/codewars/SudokuValidator.java
@@ -14,11 +14,11 @@ public class SudokuValidator {
         //iterate through columns
         int startingIndexOfColumn = 0;
         //iterate by grids
+        int columnIndex = 0;
         int startingIndexOfGrid = 0;
-        int startingIndexOfGrid2 = 0;
         int lastIndexOfGrid = 3;
         return compareByRow(sudoku) && compareByColumn(sudoku, startingIndexOfColumn)
-                && compareByGrid(sudoku, startingIndexOfGrid, lastIndexOfGrid, startingIndexOfGrid2);
+                && compareByGrid(sudoku, columnIndex, lastIndexOfGrid, startingIndexOfGrid);
     }
 
     public static boolean compareByRow(int[][] sudoku){
@@ -76,20 +76,5 @@ public class SudokuValidator {
     public static boolean compareArrays(List<Integer> arrayToCompare){
         Collections.sort(arrayToCompare);
         return SEQUENCE.equals(arrayToCompare);
-    }
-
-    public static void main(String[] args) {
-        int[][] sudoku = {
-                {5, 3, 4, 6, 7, 8, 9, 1, 2},
-                {6, 7, 2, 1, 9, 5, 3, 4, 8},
-                {1, 9, 8, 3, 4, 2, 5, 6, 7},
-                {8, 5, 9, 7, 6, 1, 4, 2, 3},
-                {4, 2, 6, 8, 5, 3, 7, 9, 1},
-                {7, 1, 3, 9, 2, 4, 8, 5, 6},
-                {9, 6, 1, 5, 3, 7, 2, 8, 4},
-                {2, 8, 7, 4, 1, 9, 6, 3, 5},
-                {3, 4, 5, 2, 8, 6, 1, 7, 9}
-        };
-        System.out.println(check(sudoku));
     }
 }
